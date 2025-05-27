30814cc4b08cb96dd84826daefea73bd6b69a2b5
greisersem <grejser.se@edu.spbstu.ru>
2025-05-27 14:00:06 +0300

    First task

---
+++ cam_with_opencv.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+ 
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    ret, frame = cap.read()
+ 
+    if not ret:
+        print("Can't receive frame")
+        break
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
