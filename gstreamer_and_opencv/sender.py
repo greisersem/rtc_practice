e79021cbcbfac4e7d8e7f5ec39197ebd0297d806
greisersem <grejser.se@edu.spbstu.ru>
2025-06-02 14:20:42 +0300

    GStreamer sender in ROS2 added

--- gstreamer_and_opencv/sender.py
+++ gstreamer_and_opencv/sender.py
@@ -1,12 +1,10 @@
-import cv2 as cv
+import os
 
-
-pipeline = "gst-launch-1.0 v4l2src !" \
-    "queue !" \
-    "videoconvert !" \
+os.system(
+    "gst-launch-1.0 v4l2src ! " \
+    "queue ! " \
+    "videoconvert ! " \
     "x264enc tune=zerolatency key-int-max=15 !" \
     "video/x-h264,profile=main !" \
     "rtph264pay pt=96 config-interval=-1 !" \
-    "udpsink host=192.168.1.158 port=5000"
-
-capture = cv.VideoCapture(pipeline, cv.CAP_GSTREAMER)
+    "udpsink host=192.168.1.158 port=5000")
